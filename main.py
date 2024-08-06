8730d9e8bffc9cddcdd1ba91b8ae6ea3d385e94e
agent192007 <king191621@gmail.com>
2024-08-06 13:15:03 +0530

    update 1

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+from turtle import Screen
+from paddle import Paddle
+import time
 
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("ping pong")
+screen.tracer(0)
+
+r_paddle = Paddle('Up', 'Down',350)
+l_paddle = Paddle('w', 's',-350)
+
+screen.update()
+
+loop = True
+
+r_paddle.paddle_loop(screen)
+l_paddle.paddle_loop(screen)
+
+screen.exitonclick()
---
+++ paddle.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+import turtle
+
+
+class Paddle(Turtle):
+    def __init__(self,up,down,x):
+        super().__init__()
+        self.shape("square")
+        self.penup()
+        self.color("white")
+        self.shapesize(stretch_wid=1, stretch_len=5)
+        self.goto(x, 0)
+        self.left(90)
+        self.up_key = up
+        self.down_key = down
+
+    def paddle_loop(self, screen):
+        def move(key):
+            if key == "up" and self.ycor() < 240:
+                self.goto(self.xcor(), self.ycor() + 10)
+            elif key == "down" and self.ycor() > -240:
+                self.goto(self.xcor(), self.ycor() - 10)
+            screen.update()
+
+        turtle.onkeypress(lambda: move("up"), self.up_key)
+        turtle.onkeypress(lambda: move("down"), self.down_key)
+        turtle.listen()
+        print(self.ycor())
